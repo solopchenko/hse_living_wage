03969b594e2d91ba008eb249452d842495ef92cf
Svyatoslav Solopchenko <svyatoslav@MacBook-Air-Svyatoslav.local>
2016-11-11 10:48:42 +0300

    Настройка url маршрутизации

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -12,5 +12,9 @@
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('living_wage.index');
+});
+
+Route::get('/test', function () {
+    return view('pages.about');
 });
